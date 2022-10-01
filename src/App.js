fdc06631c886a50f0af83b5145776bbddc65fbea
Dojeto <omlimdiwala25@gmail.com>
2022-10-01 17:08:05 +0530

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState , useEffect } from "react";
+import Register from "./Components/Register";
+import Dashboard from "./Components/Dashboard";
+import Login from "./Components/Login";
+import { Routes , Route } from "react-router-dom";
+import { Navigate } from 'react-router-dom';
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 function App() {
+  const [isAuthenticated,setIsAuthenticated] = useState(false);
+  
+  const setAuth= (boolean)=>{
+    setIsAuthenticated(boolean);
+  }
+
+  const isAuth = async()=>{
+    try {
+      const response = await fetch(`${BASE_URL}/auth/is-verify`,{
+        method:"GET",
+        headers:{token:localStorage.token}
+      })
+      const parseResponse = await response.json();
+      parseResponse === true ? setAuth(true): setAuth(false);
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+
+  useEffect(()=>{
+    console.log("Test");
+    isAuth()
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+      <Route path="/login" element={!isAuthenticated?<Login setAuth={setAuth}/>:<Navigate to="/dashboard"/>}/>
+      <Route path="/register" element={!isAuthenticated?<Register  setAuth={setAuth}/>:<Navigate to="/dashboard"/>}/>
+      <Route path="/dashboard" element={isAuthenticated?<Dashboard setAuth={setAuth}/>:<Navigate to="/login"/>}/>
+      <Route path="*" element = {<Navigate to="/login"/>}/>
+    </Routes>
+    </>
   );
 }
 
